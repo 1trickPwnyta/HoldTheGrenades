f01fc1ae87be92a53b521b388261d482eb0c28ee
1trickPonyta <1trickPonyta@kangaroostandard.com>
2021-08-29 23:36:06 -0700

    initial

---
+++ Source/HoldTheGrenadesMod.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace HoldTheGrenades
+{
+    public class HoldTheGrenadesMod : Mod
+    {
+        public const string PACKAGE_ID = "holdthegrenades.1trickPonyta";
+        public const string PACKAGE_NAME = "Hold the Grenades";
+
+        public HoldTheGrenadesMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
---
+++ Source/Patch_Pawn_DraftController.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace HoldTheGrenades
+{
+    [HarmonyPatch(typeof(Pawn_DraftController))]
+    [HarmonyPatch("<GetGizmos>b__13_1")]
+    public static class Patch_Pawn_DraftController_GetGizmos_delegate_1
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) 
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Call && (MethodInfo)instruction.operand == Refs.m_Pawn_DraftController_set_Drafted)
+                {
+                    yield return instruction;
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, Refs.f_Pawn_DraftController_pawn);
+                    yield return new CodeInstruction(OpCodes.Call, Refs.m_FireAtWillTool_SetFireAtWill);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}

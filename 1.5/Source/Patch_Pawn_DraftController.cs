20f5280f9a7f4c0be8cc7f2a0a0cf6cead9d56e5
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2025-06-03 20:31:33 -0700

    Fix caravan ambush issue

--- 1.5/Source/Patch_Pawn_DraftController.cs
+++ 1.5/Source/Patch_Pawn_DraftController.cs
@@ -1,32 +1,15 @@
 ﻿using HarmonyLib;
 using RimWorld;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
-using Verse;
 
 namespace HoldTheGrenades
 {
     [HarmonyPatch(typeof(Pawn_DraftController))]
-    [HarmonyPatch("<GetGizmos>b__15_1")]
-    public static class Patch_Pawn_DraftController_GetGizmos_delegate_1
+    [HarmonyPatch("set_Drafted")]
+    public static class Patch_Pawn_DraftController
     {
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) 
+        public static void Postfix(Pawn_DraftController __instance)
         {
-            foreach (CodeInstruction instruction in instructions)
-            {
-                if (instruction.opcode == OpCodes.Call && (MethodInfo)instruction.operand == Refs.m_Pawn_DraftController_set_Drafted)
-                {
-                    yield return instruction;
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Ldfld, Refs.f_Pawn_DraftController_pawn);
-                    yield return new CodeInstruction(OpCodes.Call, Refs.m_FireAtWillTool_SetFireAtWill);
-                }
-                else
-                {
-                    yield return instruction;
-                }
-            }
+            FireAtWillTool.SetFireAtWill(__instance.pawn);
         }
     }
 }
--- Deploy.ps1
+++ Deploy.ps1
@@ -1,4 +1,4 @@
-$path = "D:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\$(($PSScriptRoot | gi).Name)"
+$path = "$env:RIMWORLD\Mods\$(($PSScriptRoot | gi).Name)"
 
 Copy-Item "$path\About\PublishedFileId.txt" "$PSScriptRoot\About"
 Remove-Item -Recurse "$path\*"
